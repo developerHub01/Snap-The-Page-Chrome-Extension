0b9d3b90a916d58a43bb87b725faa97a9aecc921
developerHub01 <developerhubytchannel@gmail.com>
2024-05-30 21:51:52 +0600

    download functionality updated and code refector

--- popup.js
+++ popup.js
@@ -4,6 +4,10 @@ const selectAll = document.querySelector("#selectAll");
 const downloadBtn = document.querySelector(".downloadBtn");
 const selectingPortion = document.querySelector(".selectingPortion");
 const emptyImageListWarning = document.querySelector(".emptyImageListWarning");
+const numberOfImages = document.querySelector(".numberOfImages");
+const numberOfImageSelected = document.querySelector(".numberOfImageSelected");
+const downloadText = document.querySelector(".downloadText");
+const downloadingNumber = document.querySelector(".downloadingNumber");
 loader.style.display = "block";
 downloadBtn.style.pointerEvent = "none";
 selectingPortion.style.display = "none";
@@ -11,6 +15,38 @@ emptyImageListWarning.style.display = "none";
 previewGallery.style.display = "none";
 downloadBtn.style.display = "none";
 const chromeExtensionName = document.title;
+
+// get all images from host site
+const handleGetAllImages = () => {
+  // const imageValidator = (url) => {
+  //   return ["jpg", "png", "gif", "bmp", "webp", "svg"].includes(
+  //     url?.split(".").pop().split("?")[0]
+  //   );
+  // };
+  let allImages = [
+    ...new Set(
+      Array.from(document.querySelectorAll("img") || [])?.map(
+        (item) => item.src
+      )
+    ),
+  ];
+
+  allImages = allImages.filter((item) => !item.includes("fill='none'"));
+
+  let title = location.host.split(".");
+  title = title[title.length - 2] || chromeExtensionName;
+
+  return {
+    allImages,
+    title,
+  };
+};
+
+const handleSelectedNumberHandler = () => {
+  numberOfImageSelected.innerText = Array.from(
+    document.querySelectorAll(".eachImg input")
+  )?.filter((item) => item.checked).length;
+};
 
 selectAll.addEventListener("change", (e) => {
   const eachImgs = document.querySelectorAll(".eachImg");
@@ -23,33 +59,18 @@ selectAll.addEventListener("change", (e) => {
       element.classList.remove("active");
     }
   });
+  handleSelectedNumberHandler();
 });
 
-// get all images from host site
-const handleGetAllImages = () => {
-  let allImages = [
-    ...new Set([...document.querySelectorAll("img")]?.map((item) => item.src)),
-  ];
-  let hostName = location.host.split(".");
-  hostName = hostName[hostName.length - 2] || chromeExtensionName;
-  return {
-    allImages: allImages.filter((item) => {
-      const imgSrcArr = item?.split(".");
-      if (
-        ["jpg", "png", "gif", "bmp"].includes(imgSrcArr[imgSrcArr?.length - 1])
-      )
-        return item;
-    }),
-    title: hostName,
-  };
-};
-
 const handleGeneratePreview = (result) => {
+  console.log("result ============");
+  console.log(result);
   emptyImageListWarning.style.display = "none";
   if (!result || !result[0] || !result[0]["result"]) return;
   const { allImages, title } = result[0]["result"];
 
   document.querySelector(".hostSiteTitle h3").innerText = title;
+  numberOfImages.innerText = `(${allImages.length})`;
 
   if (!allImages || !allImages.length) {
     emptyImageListWarning.style.display = "block";
@@ -76,15 +97,20 @@ const handleGeneratePreview = (result) => {
       `;
   });
 
+  handleSelectedNumberHandler();
+
   document.querySelectorAll(".eachImg")?.forEach((element) => {
     const input = element.querySelector("input");
     input.addEventListener("change", (e) => {
-      if (input.checked === false) {
+      handleSelectedNumberHandler();
+      if (!input.checked) {
         selectAll.checked = false;
         element.classList.remove("active");
       } else {
         element.classList.add("active");
         selectAll.checked = true;
+
+        // if all image is selected then selectAll is checked
         document.querySelectorAll(".eachImg input").forEach((element) => {
           if (!element.checked) {
             selectAll.checked = false;
@@ -101,10 +127,20 @@ const handleDownload = async (allImages, hostTitle) => {
   const zip = new JSZip();
 
   for (let i in allImages) {
-    const res = await fetch(allImages[i]);
-    const blob = await res.blob();
-    zip.file(`${hostTitle}_${i}.png`, blob);
+    try {
+      console.log(i);
+      const res = await fetch(allImages[i]);
+      const blob = await res.blob();
+      zip.file(`${hostTitle}_${i}.png`, blob);
+
+      downloadText.innerText = "Generating image...";
+      downloadingNumber.innerText = `(${i})`;
+    } catch (error) {
+      console.log(error.message);
+    }
   }
+  downloadText.innerText = "download";
+  downloadingNumber.innerText = "";
 
   zip.generateAsync({ type: "blob" }).then((content) => {
     chrome.downloads.download({
@@ -122,7 +158,7 @@ downloadBtn.addEventListener("click", () => {
 
   const title = document.querySelector(".hostSiteTitle h3").innerText;
 
-  handleDownload(allImages, title);
+  allImages && handleDownload(allImages, title);
 });
 
 // getting current host tab and getting access for scripting.
@@ -130,6 +166,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   const activeTab = tabs[0];
 
   if (activeTab.url && activeTab.url.startsWith("chrome")) return;
+
   chrome.scripting.executeScript(
     {
       target: { tabId: activeTab.id },
@@ -137,4 +174,10 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     },
     (result) => handleGeneratePreview(result)
   );
+  // console.log(chrome);
+
+  chrome.action.onClicked.addListener((tab) => {
+    console.log(tab);
+    console.log(Date.now());
+  });
 });
